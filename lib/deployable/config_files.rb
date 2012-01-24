38ef304258f5bd5494fa3d9d0d1fd973550d5627
Flurin Egger <flurin@digitpaint.nl>
2012-01-24 10:39:23 +0100

    Update config_files module documentation

--- lib/deployable/config_files.rb
+++ lib/deployable/config_files.rb
@@ -3,8 +3,36 @@
 # Allows you to upload stage dependent config files with multiple strategies
 #
 # == Usage
+#
+# All you have to do is set the variable :config_files with the following structure:
+#
+#   set :config_files, {
+#     "test.rb"  => "#{current_release}/test.rb",
+#     "local.rb" => {
+#       :location  => "#{current_release}/config/local.rb",
+#       :strategy => :put
+#     },
+#     "skyline_configuration.rb" => {
+#       :location => "#{current_release}/config/initializers/skyline_configuration.rb",
+#       :strategy => :copy
+#     }
+#   }   
 # 
-# == Example
+# Every key of the hash above is a filename of a file that resides in <tt>:stage_dir/:stage</tt> (default=config/deploy/:stage)
+# The filename kan either be filename.xyz or filename.xyz.erb. If both a regular and an .erb variant are present, only the
+# .erb variant is processed. And you guessed right: the .erb variant is processed by ERB before putting.
+#
+# The value of the hash can either be a string or another hash. The config_file configuration hash has the folowing keys
+#
+# [:location]   The location the file will be stored on the server after deployment (final location)
+# [:strategy]   The strategy defines how the file will be stored on the server. The default strategy is :put, see below for more info
+# 
+# The most simple usage is just "filename.xyz" => "location.xyz" this will assume the default strategy of :put.
+#
+# === Strategies
+#
+# [:put]   The file will be processed (in case of ERB) and uploaded during every deploy
+# [:copy]  The file will be processed and uploaded once during setup and after that it'll be copied to the final location on each deploy
 # 
 Capistrano::Configuration.instance(true).load do
   
@@ -32,18 +60,26 @@ Capistrano::Configuration.instance(true).load do
     # Normalize config files so we can do {"filename.rb" => "location"} instead
     # of the full syntax. The default strategy is :put
     def normalize_configs
-      if configs = fetch(:config_files)
+      if configs = fetch(:config_files, {})
         configs.inject({}) do |mem, (filename, options)|
-          if options.kind_of?(String)
-            mem[filename] = {:location => options, :strategy => :put}
-          else
-            mem[filename] = options
+          if !options.kind_of?(Hash)
+            options = {:location => options, :strategy => :put}
+          end
+          mem[filename] = {}
+          options.each do |k,v|
+            mem[filename][k] = interpolate(v)
           end
           mem
         end
-      else
-        {}
       end      
+    end
+    
+    def interpolate(var)
+      if var.respond_to?(:call)
+        var.call
+      else
+        var
+      end
     end
     
     def render_config_file(file)
